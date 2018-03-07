51d5682156dbb50a45ad10a27613e0dc7e96f057
joaoperfig <joaoperfig@gmail.com>
2018-03-07 18:36:21 +0000

    add new main menu assets

--- Dwarven Wizardry Soccer Shenanigans/Assets/scripts/wizards/RandomWiz.cs
+++ Dwarven Wizardry Soccer Shenanigans/Assets/scripts/wizards/RandomWiz.cs
@@ -14,10 +14,10 @@ public class RandomWiz : MonoBehaviour {
 
 		Wizard lewi = gameObject.GetComponent<Wizard> ();
 		lewi.abilities = new Ability[4];
-		lewi.abilities [0] = new Ability[5] {new BasicAbility(lewi), new RuneBasic(lewi), new ShadowBasic(lewi), new ForceBasic(lewi), new TentacleBasic(lewi)} [Random.Range (0, 5)];
-		lewi.abilities [1] = new Ability[5] {new FireSpeed(lewi), new ShadowJump(lewi), new ForceDash(lewi), new RuneImplode(lewi), new TentacleTrail(lewi)} [Random.Range (0, 5)];
-		lewi.abilities [2] = new Ability[10] {new SmallFireBall(lewi), new BigFireBall(lewi), new ForceBall(lewi), new ForcePull(lewi), new RuneExplode(lewi), new RuneTeleport(lewi), new ShadowPossession(lewi), new ShadowSwap(lewi), new Tentacle(lewi), new TentacleGhost(lewi)} [Random.Range (0, 10)];
-		lewi.abilities [3] = new Ability[10] {new SmallFireBall(lewi), new BigFireBall(lewi), new ForceBall(lewi), new ForcePull(lewi), new RuneExplode(lewi), new RuneTeleport(lewi), new ShadowPossession(lewi), new ShadowSwap(lewi), new Tentacle(lewi), new TentacleGhost(lewi)} [Random.Range (0, 10)];
+		lewi.abilities [0] = new Ability[6] {new BasicAbility(lewi), new RuneBasic(lewi), new ShadowBasic(lewi), new ForceBasic(lewi), new TentacleBasic(lewi), new DeathBasic(lewi)} [Random.Range (0, 6)];
+		lewi.abilities [1] = new Ability[6] {new FireSpeed(lewi), new ShadowJump(lewi), new ForceDash(lewi), new RuneImplode(lewi), new TentacleTrail(lewi), new DeathJump(lewi)} [Random.Range (0, 6)];
+		lewi.abilities [2] = new Ability[12] {new SmallFireBall(lewi), new BigFireBall(lewi), new ForceBall(lewi), new ForcePull(lewi), new RuneExplode(lewi), new RuneTeleport(lewi), new ShadowPossession(lewi), new ShadowSwap(lewi), new Tentacle(lewi), new TentacleGhost(lewi), new DeathZombie(lewi), new DeathSkeleton(lewi)} [Random.Range (0, 12)];
+		lewi.abilities [3] = new Ability[12] {new SmallFireBall(lewi), new BigFireBall(lewi), new ForceBall(lewi), new ForcePull(lewi), new RuneExplode(lewi), new RuneTeleport(lewi), new ShadowPossession(lewi), new ShadowSwap(lewi), new Tentacle(lewi), new TentacleGhost(lewi), new DeathZombie(lewi), new DeathSkeleton(lewi)} [Random.Range (0, 12)];
 		
 	}
 	
---
+++ Dwarven Wizardry Soccer Shenanigans/Assets/ui/CloudScrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScrolling : MonoBehaviour {
+
+	private Vector3 initpos;
+
+	public float vel;
+
+	// Use this for initialization
+	void Start () {
+		initpos = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newpos = gameObject.transform.position;
+		newpos = newpos + new Vector3 (vel * Time.deltaTime, 0, 0);
+		gameObject.transform.position = newpos;
+		if (gameObject.transform.GetChild (0).position.x > initpos.x) {
+			gameObject.transform.position = initpos;
+		}
+	}
+}
